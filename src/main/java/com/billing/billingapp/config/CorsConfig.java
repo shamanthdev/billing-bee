72bcb4764839b882184d3e2a31382d54bd35cfb1
shamantha.hegde <shamantha.hegde@6dtech.co.in>
2026-04-02 11:10:56 +0530

    added cors config

---
+++ src/main/java/com/billing/billingapp/config/CorsConfig.java
@@ -0,0 +1,23 @@
+package com.billing.billingapp.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.*;
+
+@Configuration
+public class CorsConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("*") // allow all (for now)
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
